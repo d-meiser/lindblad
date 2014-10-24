cb18e5b2a08b67c9e1ba8dd1c7eaeada945a7aab
Dominic Meiser <dmeiser@txcorp.com>
2014-10-24 13:19:52 -0600

    Change coupling matrix element type from Amplitude to double.

--- src/qsys.cpp
+++ src/qsys.cpp
@@ -22,6 +22,6 @@ void Coupling::apply(int dim, const Amplitude *A, Amplitude *B) {
   static const Amplitude I(0, 1);
   leftApply(m, n, 0.5 * g / I, dim, A, B);
   leftApply(n, m, 0.5 * g / I, dim, A, B);
-  rightApply(n, m, -0.5 * conj(g) / I, dim, A, B);
-  rightApply(m, n, -0.5 * conj(g) / I, dim, A, B);
+  rightApply(n, m, -0.5 * g / I, dim, A, B);
+  rightApply(m, n, -0.5 * g / I, dim, A, B);
 }
