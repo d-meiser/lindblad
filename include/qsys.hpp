f572c0a4a1ae42f2da35b64b27c1014fdacc706d
Dominic Meiser <dmeiser@txcorp.com>
2014-10-23 22:26:59 -0600

    Add rightApply.

--- include/qsys.hpp
+++ include/qsys.hpp
@@ -8,5 +8,7 @@ typedef std::complex<double> Amplitude;
 void leftApply(int row, int col, Amplitude alpha, int dim, const Amplitude *A,
                Amplitude *B);
 
+void rightApply(int row, int col, Amplitude alpha, int dim, const Amplitude *A,
+               Amplitude *B);
 #endif
 
--- src/qsys.cpp
+++ src/qsys.cpp
@@ -8,3 +8,12 @@ void leftApply(int row, int col, Amplitude alpha, int dim, const Amplitude *A,
     B[c] += alpha * A[c];
   }
 }
+
+void rightApply(int row, int col, Amplitude alpha, int dim, const Amplitude *A,
+               Amplitude *B) {
+  B += col;
+  A += col;
+  for (int r = 0; r < dim; ++r) {
+    B[r * dim] += alpha * A[r * dim];
+  }
+}
