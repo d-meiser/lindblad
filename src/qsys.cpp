3811ddc5e62a766a845f489eff99c1ecba6588f5
Dominic Meiser <dmeiser@txcorp.com>
2014-10-27 19:52:59 -0600

    Fix decay operator.

--- src/qsys.cpp
+++ src/qsys.cpp
@@ -22,6 +22,13 @@ void Coupling::apply(int dim, const Amplitude *A, Amplitude *B) {
   static const Amplitude I(0, 1);
   leftApply(m, n, 0.5 * g / I, dim, A, B);
   leftApply(n, m, 0.5 * g / I, dim, A, B);
-  rightApply(n, m, -0.5 * g / I, dim, A, B);
-  rightApply(m, n, -0.5 * g / I, dim, A, B);
+  rightApply(n, m, -0.5 * conj(g) / I, dim, A, B);
+  rightApply(m, n, -0.5 * conj(g) / I, dim, A, B);
 }
+
+void Decay::apply(int dim, const Amplitude *A, Amplitude *B) {
+  leftApply(outof, outof, -0.5 * gamma, dim, A, B);
+  rightApply(outof, outof, -0.5 * gamma, dim, A, B);
+  B[into + into * dim] += gamma * A[outof + outof * dim];
+}
+
--- tests/test_Decay.cpp
+++ tests/test_Decay.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
 #include <qsys.hpp>
+#include <testUtils.hpp>
 
 TEST(Decay, Constructor) {
   Decay d(0, 1, 0.2);
 }
 
+TEST(Decay, EToG) {
+  Decay d(0, 1, 0.2);
+  std::vector<Amplitude> rho1(4, 1);
+  std::vector<Amplitude> rho2(4, 0);
+  d.apply(2, &rho1[0], &rho2[0]);
+}
