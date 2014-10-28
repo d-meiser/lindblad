9662e35fa4050e5aa17e666f4f01e3c9d6414e85
Dominic Meiser <dmeiser@txcorp.com>
2014-10-27 21:19:42 -0600

    Fix application of coupling.

--- src/qsys.cpp
+++ src/qsys.cpp
@@ -3,7 +3,7 @@
 void leftApply(int row, int col, Amplitude alpha, int dim, const Amplitude *A,
                Amplitude *B) {
   B += row * dim;
-  A += row * dim;
+  A += col * dim;
   for (int c = 0; c < dim; ++c) {
     B[c] += alpha * A[c];
   }
@@ -12,7 +12,7 @@ void leftApply(int row, int col, Amplitude alpha, int dim, const Amplitude *A,
 void rightApply(int row, int col, Amplitude alpha, int dim, const Amplitude *A,
                 Amplitude *B) {
   B += col;
-  A += col;
+  A += row;
   for (int r = 0; r < dim; ++r) {
     B[r * dim] += alpha * A[r * dim];
   }
@@ -22,8 +22,8 @@ void Coupling::apply(int dim, const Amplitude *A, Amplitude *B) {
   static const Amplitude I(0, 1);
   leftApply(m, n, 0.5 * g / I, dim, A, B);
   leftApply(n, m, 0.5 * conj(g) / I, dim, A, B);
-  rightApply(n, m, -0.5 * g / I, dim, A, B);
-  rightApply(m, n, -0.5 * conj(g) / I, dim, A, B);
+  rightApply(m, n, -0.5 * g / I, dim, A, B);
+  rightApply(n, m, -0.5 * conj(g) / I, dim, A, B);
 }
 
 void Decay::apply(int dim, const Amplitude *A, Amplitude *B) {
@@ -32,3 +32,21 @@ void Decay::apply(int dim, const Amplitude *A, Amplitude *B) {
   B[into + into * dim] += gamma * A[outof + outof * dim];
 }
 
+void MasterEqnRhs::addCoupling(Coupling c) {
+  couplings.push_back(c);
+}
+
+void MasterEqnRhs::addDecay(Decay d) {
+  decays.push_back(d);
+}
+
+void MasterEqnRhs::apply(int dim, const Amplitude *A, Amplitude *B) {
+  for (std::vector<Coupling>::iterator c = couplings.begin();
+       c != couplings.end(); ++c) {
+    c->apply(dim, A, B);
+  }
+  for (std::vector<Decay>::iterator d = decays.begin(); d != decays.end();
+       ++d) {
+    d->apply(dim, A, B);
+  }
+}
