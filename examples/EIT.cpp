402aa2ec6ed37925a19b63c2115f89ef571094aa
Dominic Meiser <dmeiser@txcorp.com>
2014-10-30 19:05:26 -0400

    Add EIT example.

---
+++ examples/EIT.cpp
@@ -0,0 +1,25 @@
+#include <qsys.hpp>
+#include <cmath>
+
+static const int numIters = 1000;
+
+int main() {
+  int dim = 4;
+  double Delta = -0.1;
+  double OmegaB = 0.01;
+  double OmegaR = 1.0;
+
+  MasterEqnRhs rhs;
+  rhs.addCoupling(Coupling(3, 0, -OmegaR / 2.0 / sqrt(6.0)));
+  rhs.addCoupling(Coupling(3, 2, -OmegaR / 2.0 / sqrt(6.0)));
+  rhs.addCoupling(Coupling(0, 0, OmegaB / 8.0));
+  rhs.addCoupling(Coupling(2, 2, -OmegaB / 8.0));
+  rhs.addCoupling(Coupling(3, 3, -Delta));
+  std::vector<Amplitude> rhoInitial(dim * dim, 0);
+  rhoInitial[1 + 1 * dim] = 1.0;
+  MasterEquation meqn(dim, &rhoInitial[0], &rhs);
+  for (int i = 0; i < numIters; ++i) {
+    meqn.takeStep();
+  }
+  return 0;
+}
