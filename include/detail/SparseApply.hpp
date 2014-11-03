d49307034924a4029e1ebe7b8ef11c867181ac37
Dominic Meiser <dmeiser@txcorp.com>
2014-11-02 17:28:36 -0700

    Add copyright statements to files.

--- include/detail/SparseApply.hpp
+++ include/detail/SparseApply.hpp
@@ -1,3 +1,21 @@
+/*
+Copyright 2014 Dominic Meiser
+
+This file is part of lindblad.
+
+lindblad is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+lindblad is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with lindblad.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef SPARSE_APPLY_HPP
 #define SPARSE_APPLY_HPP
 
