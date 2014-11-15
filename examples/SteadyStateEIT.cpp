0b24c6a528456baddb55b691fdecb6d3a42c7321
Dominic Meiser <dmeiser@txcorp.com>
2014-11-15 11:13:15 -0700

    Add command line options to EIT example.

--- examples/SteadyStateEIT.cpp
+++ examples/SteadyStateEIT.cpp
@@ -1,4 +1,12 @@
-static const char help[] = "Computation of steady state of a master eqaution.";
+static const char help[] =
+    "Computation of steady state of a master eqaution.\n"
+    "Usage:\n"
+    "SteadyStateEIT:\n"
+    "Compute steady state with default parameters\n"
+    "SteadyStateEIT OmegaR OmegaB Delta gamma Gamma deltaB:\n"
+    "Compute steady state with given parameters. All parameters must be "
+    "provided\n"
+    "in this form. Provide a floating point number for each parameters.";
 #include <MasterEqn.hpp>
 #include <Amplitude.hpp>
 
@@ -6,6 +14,15 @@ static const char help[] = "Computation of steady state of a master eqaution.";
 #include <petscksp.h>
 
 static const PetscInt N = 4;
+
+struct SystemParameters {
+  PetscReal OmegaR, OmegaB, Delta, gamma, deltaB, Gamma;
+};
+
+static PetscErrorCode getParameters(int argn, const char** argv,
+                                    SystemParameters* parameters);
+static PetscErrorCode readParameter(int n, const char** argv, PetscReal* param,
+                                    const char* name);
 
 #undef __FUNCT__
 #define __FUNCT__ "mult"
@@ -27,18 +44,21 @@ int mult(Mat A, Vec x, Vec y) {
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
-int main(int argn, char **argv) {
-  KSP            ksp;
-  PC             pc;
-  Vec            x, y;
-  Mat            A;
-  PetscInt       i, m, n;
-  PetscErrorCode ierr;
-  PetscReal      OmegaR, OmegaB, Delta, gamma, deltaB, Gamma;
-  MasterEqn      meqn(N);
-  PetscScalar    *xarr, trace;
+int main(int argn, const char** argv) {
+  KSP              ksp;
+  PC               pc;
+  Vec              x, y;
+  Mat              A;
+  PetscInt         i, m, n;
+  PetscErrorCode   ierr;
+  MasterEqn        meqn(N);
+  PetscScalar      *xarr, trace;
+  SystemParameters params;
 
-  PetscInitialize(&argn, &argv, 0, help);
+  PetscFunctionBegin;
+  PetscInitialize(&argn, (char***)&argv, 0, help);
+
+  getParameters(argn, argv, &params);
   ierr = VecCreate(PETSC_COMM_WORLD, &x);CHKERRQ(ierr);
   ierr = VecSetSizes(x, PETSC_DECIDE, N * N);CHKERRQ(ierr);
   ierr = VecSetFromOptions(x);CHKERRQ(ierr);
@@ -54,28 +74,21 @@ int main(int argn, char **argv) {
   ierr = VecGetLocalSize(y, &m);CHKERRQ(ierr);
   ierr = VecGetLocalSize(x, &n);CHKERRQ(ierr);
 
-  OmegaR = 1.25e6 * 2.0 * M_PI;
-  OmegaB = 700.0e3 * 2.0 * M_PI * 0.01;
-  Delta = 0.0;
-  gamma = 1.0e3 * 2.0 * M_PI;
-  Gamma = 6.0e6 * 2.0 * M_PI;
-  /* 700kHz/Gauss, 10 milli Gauss */
-  //deltaB = 700.0e3 * 2.0 * M_PI * 0.01;
-  deltaB = 0.0;
-  meqn.addCoupling(0, 0, OmegaB / 8.0);
-  meqn.addCoupling(0, 1, deltaB / 8.0 / sqrt(2.0));
-  meqn.addCoupling(1, 2, deltaB / 8.0 / sqrt(2.0));
-  meqn.addCoupling(2, 2, -OmegaB / 8.0);
-  meqn.addCoupling(0, 3, Amplitude(1.0, -1.0) / 8.0 * OmegaR / sqrt(3.0));
-  meqn.addCoupling(2, 3, -Amplitude(1.0, 1.0) / 8.0 * OmegaR / sqrt(3.0));
-  meqn.addCoupling(3, 3, -Delta);
-  meqn.addDecay(0, 0, gamma);
-  meqn.addDecay(1, 1, gamma);
-  meqn.addDecay(2, 2, gamma);
-  meqn.addDecay(3, 3, gamma);
-  meqn.addDecay(0, 3, Gamma);
-  meqn.addDecay(1, 3, Gamma);
-  meqn.addDecay(2, 3, Gamma);
+
+  meqn.addCoupling(0, 0, params.OmegaB / 8.0);
+  meqn.addCoupling(0, 1, params.deltaB / 8.0 / sqrt(2.0));
+  meqn.addCoupling(1, 2, params.deltaB / 8.0 / sqrt(2.0));
+  meqn.addCoupling(2, 2, -params.OmegaB / 8.0);
+  meqn.addCoupling(0, 3, Amplitude(1.0, -1.0) / 8.0 * params.OmegaR / sqrt(3.0));
+  meqn.addCoupling(2, 3, -Amplitude(1.0, 1.0) / 8.0 * params.OmegaR / sqrt(3.0));
+  meqn.addCoupling(3, 3, -params.Delta);
+  meqn.addDecay(0, 0, params.gamma);
+  meqn.addDecay(1, 1, params.gamma);
+  meqn.addDecay(2, 2, params.gamma);
+  meqn.addDecay(3, 3, params.gamma);
+  meqn.addDecay(0, 3, params.Gamma);
+  meqn.addDecay(1, 3, params.Gamma);
+  meqn.addDecay(2, 3, params.Gamma);
   ierr = MatCreateShell(PETSC_COMM_WORLD, m, n, N * N, N * N, &meqn, &A);CHKERRQ(ierr);
   ierr = MatShellSetOperation(A, MATOP_MULT, (void(*)(void))mult);CHKERRQ(ierr);
 
@@ -105,3 +118,42 @@ int main(int argn, char **argv) {
   ierr = VecDestroy(&y);CHKERRQ(ierr);
   PetscFinalize();
 }
+
+#undef __FUNCT__
+#define __FUNCT__ "getParameters"
+PetscErrorCode getParameters(int argn, const char** argv,
+                             SystemParameters* parameters) {
+  PetscFunctionBegin;
+  if (argn == 1) {
+    parameters->OmegaR = 1.25e6 * 2.0 * M_PI;
+    parameters->OmegaB = 700.0e3 * 2.0 * M_PI * 0.01;
+    parameters->Delta = 0.0;
+    parameters->gamma = 1.0e3 * 2.0 * M_PI;
+    parameters->Gamma = 6.0e6 * 2.0 * M_PI;
+    /* 700kHz/Gauss, 10 milli Gauss */
+    // deltaB = 700.0e3 * 2.0 * M_PI * 0.01;
+    parameters->deltaB = 0.0;
+  } else if (argn < 7) {
+    SETERRQ(PETSC_COMM_WORLD, 1, "Insufficient parameters provided.");
+  } else {
+    readParameter(1, argv, &parameters->OmegaR, "OmegaR");
+    readParameter(2, argv, &parameters->OmegaB, "OmegaB");
+    readParameter(3, argv, &parameters->Delta, "Delta");
+    readParameter(4, argv, &parameters->gamma, "gamma");
+    readParameter(5, argv, &parameters->Gamma, "Gamma");
+    readParameter(6, argv, &parameters->deltaB, "deltaB");
+  }
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "readParameter"
+PetscErrorCode readParameter(int n, const char** argv, PetscReal* param,
+                    const char* name) {
+  PetscFunctionBegin;
+  if (sscanf(argv[n], "%lf", param) != 1) {
+    SETERRQ2(PETSC_COMM_WORLD, 1, "Failed to read argument %d: %s\n", n, name);
+  }
+  PetscFunctionReturn(0);
+}
+
