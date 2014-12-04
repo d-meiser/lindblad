7af8e0e370b6002cd6fb255bd16125b6967af355
Dominic Meiser <dmeiser@txcorp.com>
2014-12-03 20:24:01 -0700

    Apply fixes from SteadyStateEIT.cpp to EIT.cpp.

--- examples/EIT.cpp
+++ examples/EIT.cpp
@@ -19,27 +19,77 @@ with lindblad.  If not, see <http://www.gnu.org/licenses/>.
 #include <Lindblad.hpp>
 #include <cmath>
 #include <vector>
+#include <cstdio>
 
 static const int numIters = 1000;
 
-int main() {
+struct SystemParameters {
+  double OmegaR, OmegaB, Delta, gamma, Gamma, deltaB;
+};
+
+int readParameter(int n, const char** argv, double* param,
+                  const char* name);
+int getParameters(int argn, const char** argv, SystemParameters* parameters);
+
+int main(int argn, const char** argv) {
   int dim = 4;
-  double Delta = -0.1;
-  double OmegaB = 0.01;
-  double OmegaR = 1.0;
+  SystemParameters params;
+  getParameters(argn, argv, &params);
 
   MasterEqn meqn(dim);
-  meqn.addCoupling(3, 0, -OmegaR / 2.0 / sqrt(6.0));
-  meqn.addCoupling(3, 2, -OmegaR / 2.0 / sqrt(6.0));
-  meqn.addCoupling(0, 0, OmegaB / 8.0);
-  meqn.addCoupling(2, 2, -OmegaB / 8.0);
-  meqn.addCoupling(3, 3, -Delta);
+  meqn.addCoupling(0, 0, params.OmegaB / 2.0);
+  meqn.addCoupling(0, 1, params.deltaB / 2.0 / sqrt(2.0));
+  meqn.addCoupling(1, 2, params.deltaB / 2.0 / sqrt(2.0));
+  meqn.addCoupling(2, 2, -params.OmegaB / 2.0);
+  meqn.addCoupling(0, 3, 1.0 / 4.0 * params.OmegaR / sqrt(6.0));
+  meqn.addCoupling(2, 3, -1.0 / 4.0 * params.OmegaR / sqrt(6.0));
+  meqn.addCoupling(3, 3, -params.Delta);
+  meqn.addDecay(0, 0, params.gamma);
+  meqn.addDecay(1, 1, params.gamma);
+  meqn.addDecay(2, 2, params.gamma);
+  meqn.addDecay(3, 3, params.gamma);
+  meqn.addDecay(0, 3, params.Gamma / 3.0);
+  meqn.addDecay(1, 3, params.Gamma / 3.0);
+  meqn.addDecay(2, 3, params.Gamma / 3.0);
 
   std::vector<Amplitude> rhoInitial(dim * dim, 0);
   rhoInitial[1 + 1 * dim] = 1.0;
   MasterEqnEvolution evolution(meqn, &rhoInitial[0]);
   for (int i = 0; i < numIters; ++i) {
     evolution.takeStep();
+  }
+  return 0;
+}
+
+int getParameters(int argn, const char** argv, SystemParameters* parameters) {
+  int ierr;
+
+  if (argn == 1) {
+    parameters->OmegaR = 1.25e6 * 2.0 * M_PI;
+    parameters->OmegaB = 700.0e3 * 2.0 * M_PI * 0.01;
+    parameters->Delta = 0.0;
+    parameters->gamma = 1.0e3 * 2.0 * M_PI;
+    parameters->Gamma = 6.0e6 * 2.0 * M_PI;
+    parameters->deltaB = 0.0;
+  } else if (argn < 7) {
+    printf("Insufficient parameters provided.");
+    return -1;
+  } else {
+    ierr = readParameter(1, argv, &parameters->OmegaR, "OmegaR");
+    ierr = readParameter(2, argv, &parameters->OmegaB, "OmegaB");
+    ierr = readParameter(3, argv, &parameters->Delta, "Delta");
+    ierr = readParameter(4, argv, &parameters->gamma, "gamma");
+    ierr = readParameter(5, argv, &parameters->Gamma, "Gamma");
+    ierr = readParameter(6, argv, &parameters->deltaB, "deltaB");
+  }
+  return ierr;
+}
+
+int readParameter(int n, const char** argv, double* param,
+                  const char* name) {
+  if (sscanf(argv[n], "%lf", param) != 1) {
+    printf("Failed to read argument %d: %s\n", n, name);
+    return -1;
   }
   return 0;
 }
