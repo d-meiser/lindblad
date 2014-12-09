7a1f3200c3e486baa643d1f3aa1a4bb270a851f3
Dominic Meiser <dmeiser@txcorp.com>
2014-12-09 11:12:55 -0500

    Use joblib for parallelism if available.

--- examples/scanEIT.py
+++ examples/scanEIT.py
@@ -7,6 +7,15 @@ from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 import matplotlib.pyplot as plt
+try:
+    import joblib
+    n_jobs = 4
+    print "Using joblib with", n_jobs, "processes."
+    have_joblib = True
+except ImportError:
+    print "joblib not available."
+    have_joblib = False
+    
 
 if (len(sys.argv) == 2):
 	# one argument has been provided for output plot name
@@ -44,6 +53,17 @@ def rotation(polarizationArray):
 	# to get light polarization rotation from atomic polarization
 	return polarizationArray[:,0].real + polarizationArray[:,1].real
 
+def compute_polarization(OmegaR, OmegaBs, Delta, gamma, Gamma, deltaB):
+    if have_joblib:
+        polarizations = joblib.Parallel(n_jobs = n_jobs)(
+                joblib.delayed(get_polarization)(
+                    [OmegaR, ob, Delta, gamma, Gamma, deltaB])
+                for ob in OmegaBs)
+    else:
+        polarizations = [get_polarization([OmegaR, ob, Delta, gamma, Gamma, deltaB]) for ob in OmegaBs]
+    polarizations = np.array(polarizations)
+    return polarizations
+
 def main(argv):
     OmegaR = 1.25e6 * 2.0 * np.pi
     Delta = 0.0;
@@ -52,14 +72,12 @@ def main(argv):
     OmegaB = np.arange(-0.1, 0.1, 0.0005) * 700.0e3 * 2.0 * np.pi
 
     deltaB = 700.0e3 * 2.0 * np.pi * 0.01
-    NonZeroPolarization=np.array([get_polarization([OmegaR, ob, Delta, gamma, Gamma, deltaB])
-                        for ob in OmegaB])
+    NonZeroPolarization = compute_polarization(OmegaR, OmegaB, Delta, gamma, Gamma, deltaB)
     absorptionNonZeroField = absorption(NonZeroPolarization)
     rotationNonZeroField = rotation(NonZeroPolarization)
 
     deltaB = 700.0e3 * 2.0 * np.pi * 0.0
-    ZeroPolarization=np.array([get_polarization([OmegaR, ob, Delta, gamma, Gamma, deltaB])
-                        for ob in OmegaB])
+    ZeroPolarization = compute_polarization(OmegaR, OmegaB, Delta, gamma, Gamma, deltaB)
     absorptionZeroField = absorption(ZeroPolarization)
     rotationZeroField = rotation(ZeroPolarization)
 
