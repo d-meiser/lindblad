11d1a0c115b5c59f7790ef3f354b4a89a9230532
Dominic Meiser <dmeiser@txcorp.com>
2015-02-04 21:33:46 -0700

    Move _USE_MATH_DEFINES to before first include.

--- examples/EIT.cpp
+++ examples/EIT.cpp
@@ -16,8 +16,8 @@ for more details.
 You should have received a copy of the GNU General Public License along
 with lindblad.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <Lindblad.hpp>
 #define _USE_MATH_DEFINES
+#include <Lindblad.hpp>
 #include <cmath>
 #include <vector>
 #include <cstdio>
