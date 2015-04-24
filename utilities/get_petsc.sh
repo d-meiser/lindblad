a41c9c1e2029e427a7892460a45fb51d790b1b97
Dominic Meiser <dmeiser@txcorp.com>
2015-04-24 19:40:05 -0400

    Fix the script for downloading and building petsc.

--- utilities/get_petsc.sh
+++ utilities/get_petsc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.5.3.tar.gz
-tar xfz petsc-3.5.2.tar.gz
-cd petsc-3.5.2
+tar xfz petsc-lite-3.5.3.tar.gz
+cd petsc-3.5.3
 PETSC_ARCH=cpucmplx-rel PETSC_DIR=`pwd` ./configure \
   --with-scalar-type=complex \
   --with-mpi=0 \
@@ -12,7 +12,7 @@ PETSC_ARCH=cpucmplx-rel PETSC_DIR=`pwd` ./configure \
   --with-fortran-kernels=0 \
   --with-pthread=0 \
   --with-mpiuni-fortran-binding=0 \
-  --with-fortran-interfaces
+  --with-fortran-interfaces=0
 make PETSC_DIR=`pwd` PETSC_ARCH=cpucmplx-rel all
 make PETSC_DIR=`pwd` PETSC_ARCH=cpucmplx-rel install
 cd -
