2522b34f12541b4b97511d95486185e0ae45219e
Dominic Meiser <dmeiser@txcorp.com>
2015-05-11 15:25:05 -0400

    Add a script for building an optimized version of PETSc.

--- utilities/get_petsc_rel.sh
+++ utilities/get_petsc_rel.sh
@@ -2,19 +2,18 @@
 wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.5.3.tar.gz
 tar xfz petsc-lite-3.5.3.tar.gz
 cd petsc-3.5.3
-PETSC_ARCH=cpucmplx PETSC_DIR=`pwd` ./configure \
-  --with-debugging=1 \
+PETSC_ARCH=cpucmplx-rel PETSC_DIR=`pwd` ./configure \
   --with-scalar-type=complex \
   --with-mpi=0 \
   --with-shared-libraries=0 \
-  --prefix=../petsc-cpucmplx \
+  --prefix=../petsc-cpucmplx-rel \
   --with-x=0 \
   --with-ssl=0 \
   --with-fortran-kernels=0 \
   --with-pthread=0 \
   --with-mpiuni-fortran-binding=0 \
   --with-fortran-interfaces=0
-make PETSC_DIR=`pwd` PETSC_ARCH=cpucmplx all
-make PETSC_DIR=`pwd` PETSC_ARCH=cpucmplx install
+make PETSC_DIR=`pwd` PETSC_ARCH=cpucmplx-rel all
+make PETSC_DIR=`pwd` PETSC_ARCH=cpucmplx-rel install
 cd -
 
